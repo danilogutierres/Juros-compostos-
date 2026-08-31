4303510ac9c2f033e8f1b7d867abdd9702a10b5a
danilogutierres <daniloxdxd@gmail.com>
2026-08-31 03:27:12 -0300

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juroscompostos.simulador',
+  appName: 'Juros Compostos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
