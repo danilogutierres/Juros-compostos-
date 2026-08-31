e8f8a3d41985851fb13e0da502cbf49057cc3c97
danilogutierres <daniloxdxd@gmail.com>
2026-08-31 03:30:03 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "juros-compostos-v2";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copia = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copia));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(response =>
+          response || caches.match("./index.html")
+        )
+      )
+  );
+});
